4b8732287a263f138b913467e8a5456fc3ba2be4
Jesse Kipsang <kipsangjesse17@gmail.com>
2025-04-01 11:56:26 +0300

    Updated the index.js to include code function to replace text as instructed

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', () => {
+    let targetElement = document.querySelector('#text');
+    if (targetElement) {
+        targetElement.innerHTML = 'This is really cool!';
+    }
+})
